77fc8c1d35d3d7bcdd740d8e6e715f193a69c3b2
FastLite <Volodin_vlad2002@mail.ru>
2020-04-02 19:11:34 -0400

    Sprites are added, level 1 and 2 created and main menu changed

---
+++ Assets/Scripts/Force.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Force : MonoBehaviour
+{
+    public bool isForceAdded = false;
+    public float force = 10;
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyUp(KeyCode.Space) && !isForceAdded)
+            rb2d.AddForce(transform.up * force);
+        isForceAdded = true;
+
+    }
+
+
+}
+
---
+++ Assets/Scripts/LevelScroller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelScroller : MonoBehaviour
+{
+    public float scrollSpeed = -50;
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        float translationY = scrollSpeed * Time.deltaTime;
+        transform.Translate(0, translationY, 0);
+    }
+}
